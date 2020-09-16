fc30261636f6ffa148ae67457e7db3b446257f4f
Kristine Pray <datsun280zx@gmail.com>
2020-09-16 09:05:55 -0400

    Add all my files

---
+++ 8ball.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import random
+
+responses = [
+    "It is certain",
+    "Without a doubt",
+    "You may rely on it",
+    "Yes definitely",
+    "It is decidedly so",
+    "As I see it, yes",
+    "Most likely",
+    "Yes",
+    "Outlook good",
+    "Signs point to yes",
+    "Reply hazy try again",
+    "Better not tell you now",
+    "Ask again later",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "Outlook not so good",
+    "My sources say no",
+    "Very doubtful",
+    "My reply is no"
+]
+
+input("Tell the magic 8 ball your question:")
+answer = random.choice(responses)
+print(f"The magic 8 ball says {answer}")
