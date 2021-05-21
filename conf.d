1d904f42ac206eea3e57a249b9f21d8c107fb5b2
kristinepray <43075118+kristinepray@users.noreply.github.com>
2021-05-21 10:44:19 -0400

    Update conf.d

--- conf.d
+++ conf.d
@@ -65,7 +65,7 @@ http {
         access_log   /var/log/nginx.access_log  main;
 
         location / {
-            proxy_pass         http://127.0.0.1:8000;
+            proxy_pass         http://127.0.0.1:3000;
 
             proxy_set_header   Host             $host;
             proxy_set_header   X-Real-IP        $remote_addr;
